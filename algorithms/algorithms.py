e07f25148672549ecbd3c56717ed503da2285282
Abdurrahman Qureshi <rahman.a.qureshi@gmail.com>
2019-03-20 16:41:41 -0400

    implement levenburg least squares to estimate position, wrote a test

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -1,29 +1,32 @@
 import scipy.optimize
 import numpy as np
 
-def computeDistance(measurements, anchors):
+n = 1.4267
+A = -59.0636
+
+def compute_rssi(distance):
+    return -10*n*np.log(distance) + A
+
+def compute_distance(measurements, anchors):
     """Computes the distance of a point from the corresponding anchor of a measurement
     """
-    n = 1.4267
-    A = -59.0636
     rssi = np.array([m.rssi for m in measurements])
-    distance = np.exp((A-rssi)/(10*n));
+    distance = np.exp((A-rssi)/(10*n))
     return distance
 
-def computePosition(measurements, anchors):
+def compute_position(measurements, anchors):
     """
     Parameters:
     measurements - array - array of measurements
     anchors - map - anchors
     """
-    distances = computeDistance(measurements, anchors)
-    distances.reshape(len(distances), 1)
-    anchorX = np.zeros((len(measurements), 1))
-    anchorY = np.zeros((len(measurements, 1))
+    distances = compute_distance(measurements, anchors)
+    anchorX = np.zeros(len(measurements))
+    anchorY = np.zeros(len(measurements))
     for i in range(0, len(measurements)):
-        anchorX[i,0] = anchors[measurements.anchorId].X
-        anchorY[i,0] = anchors[measurements.anchorId].Y 
+        anchorX[i] = anchors[measurements[i].anchorId].X
+        anchorY[i] = anchors[measurements[i].anchorId].Y 
     def lossfct(x):
         return distances**2 - (x[0]-anchorX)**2 - (x[1]-anchorY)**2
-   x0 = np.array([0,0])
-   return scipy.optimize.least_squares(lossfct, x0)
+    x0 = np.array([0,0])
+    return scipy.optimize.least_squares(lossfct, x0).x
